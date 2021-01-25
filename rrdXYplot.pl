c058c154c13d94dd879280aead6cea040c38dae9
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-25 16:32:23 +0100

    boilerplate XY plotting

---
+++ rrdXYplot.pl
@@ -0,0 +1,281 @@
+#!/usr/bin/perl
+#
+# extract data from rrd and write csv data
+# TODO transfer from cmdline tool to cgi
+#
+our $usage = <<"EOF_USAGE";
+usage: $0?rrd=./path/to/my.rrd[&asdf[&bar=foo[&....
+	help debug=        	rrd= CF= start= end= step= valid_rows=
+	out= conten_type= header time= sep= delim=  tzoffset= mysqltime= humantime=
+EOF_USAGE
+
+
+
+our $usage_long = <<"EOF_USAGE_L";
+$0:
+
+retrieve data from RRD and output them as CSV to file or browser 
+
+$usage 
+
+	for further details, see RRDtool fetch for details
+	and r_ead t_he f_unny s_sourcecode
+
+params implemented (or close to....)
+	(may be you have to play with urlescaping)
+	
+	rrd=db.rrd	
+		rrd file name to retrieve data from
+		relative to script or absolute in server namespace
+
+	CF=LAST	rrd CF (AVERAGE,MIN,MAX,LAST)
+		defaults to AVERAGE
+
+	start=starttime
+		transparently forwarded to RRDtool, 
+		default NOW - 1 day
+
+	end=endtime
+		transparently forwarded to RRDtool,
+		default NOW
+	
+	step=sss 
+		resolution (seconds per value)
+		default is highest available in rrd
+
+	align
+		adjust starttime to resolution
+
+	valid_rows=n
+		preselect rows by NaN'niness
+		(integer) minimum valid fields i.e not NaN per row
+		0 - include all empty (NaN only) rows
+		1 - (default ) at least one not-NaN - don't loose any information
+		up to num-cols - fine tune information vs data throughput
+		negative integers: complement count top down e.g.
+		-1 - zero NaN allowed
+		-2 - one NaN allowed
+
+	out=output.csf
+		default ist send to browser if omitted
+		be sure that the server has write permissions
+
+	sep=;	CSV field separator, default is  ';'
+
+	delim=	CSV field delimiter, default is ''
+
+	header	include header tag line
+
+	time=timetag	
+		header line time tag, default ist 'time'
+
+	humantime	
+		translate unixtime to H_uman readable time
+
+	mysqltime 	
+		translate unixtime to MySQL parseable timestamps
+
+	tzoffset [TODO]	
+		set timezone offset, default is to use system locale
+
+	help
+		print this message
+
+	debug=3
+		set verbosity level, levels may be configured in the source
+
+EOF_USAGE_L
+
+
+# real stuff starting here ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
+
+# time zone - this is crude, would it be better as option?
+# $ENV{"TZ"} = 'local';
+# system "setenv TZ local";
+
+# our $timezone = 
+
+
+use warnings;
+use strict;
+use CGI();
+use Time::Piece();
+# use Data::Dumper::Simple ;
+# use Getopt::Std;
+use  RRDs;
+# use DateTime;
+# use Data::Dumper  ;
+# use Data::Dumper::Simple  ; conditionally on debug only
+use Cwd 'abs_path'   ;
+
+
+my $debug_default = 3;
+my $test_rrd = '/var/lib/collectd/rrd/kellerkind.rosner.lokal/cpu-0/percent-idle.rrd' ;
+my $dt_format = '%F %T' ;
+
+my $q = CGI->new;
+my %q_all_params = $q->Vars ;
+our $debug  = (defined $q_all_params{debug}) ?  $q->param('debug')  : $debug_default ;
+if ($debug) { use Data::Dumper::Simple ;}
+
+
+my $rrd =  ($q->param('rrd') ||  $test_rrd ) ;
+my $cf = $q->param('CF') || 'AVERAGE' ; 
+
+my_die (' missing parameters ' , $usage) unless %q_all_params ;
+my_die (' usage instructions: ' , $usage_long) 
+	if (defined $q_all_params{help}  or  defined $q_all_params{keywords}    ) ;
+
+# die "$usage" unless $rrdfile;
+# die "$usage_long" if ( ! ($cf) ) or $rrdfile eq '-h' or $cf eq '-h' ;
+
+# my $retval = getopts('s:e:tT:HMx:d:r:af:HMv:V:hz:')  ;
+# die "$usage" unless ($retval) ;
+# die "$usage_long" if $opt_h  ;
+
+my $start  = $q->param('start')  || 'e-1d';
+my $end    = $q->param('end')  || 'n';
+my $header = (defined $q_all_params{ header }) ;
+my $hl_timetag =  $q->param('time') || 'time' ;
+my $sep    = $q->param('sep')  || ';' ; 
+my $delim  = $q->param('delim')  || '';
+my $align  = (defined $q_all_params{ align }) ;
+my $res    = $q->param('step') || 0  ;
+my $outfile = $q->param('out') || ''   ;
+# $debug = $opt_v unless $opt_v eq ''; 
+
+# my $valid_rows = 1 ;
+# unless  ($opt_V eq '') {  $valid_rows = $opt_V ;  }
+
+my $valid_rows = (defined $q_all_params{ valid_rows })  ? $q->param('valid_rows') : 1 ;
+
+my $ct;
+if (defined $q_all_params{ content_type }) {
+	$ct = $q->param('content_type' );
+	$debug = 0 ;  # don't clobber other drains than browser window
+} else {
+	$ct = 'text/plain';
+}
+
+
+
+
+# after this header we may print pretty much anything
+print $q->header(-type =>  $ct,  -charset => 'utf8' );
+
+# my $rrdfile = "noclue";
+# debug_printf (3, "parameter db=%s CF=%s start=%s end=%s resolution=%s align=%d output=%s header=%s sep=%s delim=%s \n",
+# 	$rrdfile, $cf, $start, $end, $res, $align, $outfile, $header , $sep, $delim      );
+
+print Dumper ($q) if  $debug >=3 ;
+
+# my $rrdfile = abs_path($rrd);
+my $rrdfile = `ls $rrd`;
+chomp $rrdfile;
+# print Dumper ($rrd, $rrdfile);
+
+print Dumper ($rrdfile, $cf, $start, $end, $res, $align, $outfile, $header , $sep, $delim      ) if $debug >=3 ;
+
+# collect parameters for database call
+my @paramlist = ($rrdfile, $cf, '-s', $start, '-e', $end);
+push @paramlist, '-a' if $align ;
+push @paramlist, ('-r', $res ) if $res ; 
+
+print  Dumper ( @paramlist ) if $debug >=3 ;
+
+# ====== call the database ========
+my ($rrd_start,$step,$names,$data)  = RRDs::fetch (@paramlist); 
+
+my $namlen = $#$names;
+my $datlen = $#$data;
+
+my $dt = Time::Piece->new( $rrd_start);
+# shall we keep timezoning?
+# $dt->tzoffset = $q->param('tzoffset' ) if defined $q_all_params{ 'tzoffset' } ;
+my $dt_hr = $dt->strftime($dt_format) ;
+print  Dumper ( RRDs::error, $rrd_start,$step, $namlen, $datlen , $dt_hr ) if $debug >=3 ;
+
+# pre-process -V option ... valid rows - map the complement format
+# print  Dumper ('before' , $valid_rows);
+if ( $valid_rows < 0 ) { $valid_rows = $#$names + $valid_rows +1 ; }
+# print  Dumper ('after' , $valid_rows);
+
+# debug_printf (3, "total cols: %d - lower limit for valid Data points per row : %d \n ", $#$names , $valid_rows );
+
+# exit;
+# ---- do your work ----
+#
+if ( $outfile) {
+  open (OF , '>' ,   $outfile)  or die "$! \n could not open $outfile for writing"; 
+} else {
+  # way to redirect OF to STDOUT
+  *OF = *STDOUT;
+}
+
+# debug_printf ( 3, "opened output file: %s\n", $outfile ); 
+
+# conditional header - see -t option
+#
+if ($header) { 
+   my $titleline = my_join ( $delim, $sep, $hl_timetag , @$names) ;
+   print  OF $titleline . "\n";
+}
+
+
+# exit;
+# my $timezone = main loop over data rows, we count by index to keep close to metal
+for my $rowcnt (0 .. $#$data ) {
+   my $datarow = $$data[ $rowcnt ];			# the real data
+   my $rowtime = $rrd_start + $rowcnt * $step;		# time is calculated
+
+   # skip for data row's with too many NaN s
+   my $defcnt = 0 ;
+   foreach ( @$datarow )  {  $defcnt++ if defined $_ }
+   next unless ($defcnt >= $valid_rows) ;
+
+   # time string format selection
+   my $timestring;
+
+   if ( defined $q_all_params{mysqltime} ) {
+      my $dtr = Time::Piece->new($rowtime); 
+      # mysql datetime format YYYY-MM-DD HH:MM:SS
+      $timestring =  sprintf ( "%s %s", $dtr->ymd , $dt->hms ) ;
+   } elsif ( defined $q_all_params{humantime} ) {   #   (  ) {  
+      # human readable datetime e.g. 22.12.2020-05:00:00 , i.e. dd.mm.yyyy-hh:mm:ss
+      my $dtr = Time::Piece->new($rowtime);
+      $timestring =  sprintf ( "%s-%s", $dtr->dmy('.') , $dtr->hms );
+   } else {
+     $timestring = sprintf "%s" , $rowtime ;
+   }
+
+   my $dataline = my_join ( $delim, $sep, $timestring, @$datarow ) ;
+   print  OF $dataline . "\n";
+} 
+
+close OF if ( $outfile) ;
+
+exit ;
+
+#=========================================
+
+# my_join : extended join with delim and seperators
+# my_join ( delim, sep, @stuff )
+sub my_join {
+  my $delim = shift  @_ ;
+  my $sep   = shift  @_ ;
+  my $rv  =   return join ( $sep, map { sprintf ( "%s%s%s", $delim, $_ ,$delim) } @_ ) ;
+  return $rv ;
+}
+
+# resemble "die", supply ($message $usage)
+sub my_die {
+	my ($msg, $usage) = @_ ;
+	print $q->header(-type =>  'text/html',  -charset => 'utf8' );
+	print "<html><pre>";
+	print "\n$msg\n";
+	print "============================================================" . "\n";
+	print $usage ;
+	print "</pre></html>";
+	exit;
+
+}
